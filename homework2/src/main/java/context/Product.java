6aa28da007b62024007430d4c1637352058ba0d5
Svetlana1402 <sveta.artemeva.98@mail.ru>
2021-11-08 03:39:54 +0300

    homework2

---
+++ homework2/src/main/java/context/Product.java
@@ -0,0 +1,43 @@
+package context;
+
+public class Product {
+    Long id;
+    String title;
+    int cost;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Product(long id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + title + " " + cost;
+    }
+}
